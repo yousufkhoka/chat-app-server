ce6614556fd80aced26a83766d514e418f0b7294
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2023-04-10 00:43:51 -0700

    change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 9000;
+const port =  9000;
 
 // Bind the router db to the app
 server.db = router.db;
